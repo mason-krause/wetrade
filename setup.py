b0c5007c89bb4bdfaf68ad40f8dafad72eafc554
Mason Krause <masonkrause+github@gmail.com>
2024-06-01 16:58:42 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as f:
 
 setuptools.setup(
   name = 'wetrade',
-  version = '0.1.1',
+  version = '0.1.2',
   author = 'Mason Krause',
   description = 'An E-Trade python library built for active stock trading',
   long_description = long_description,
