693aeca4c6a212b46578a444328899ecbcc2c46a
Mason Krause <masonkrause+github@gmail.com>
2026-08-14 14:13:34 -0400

    update to support python 3.14

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
   python_requires = '>=3.7',
   install_requires = [
     'authlib',
-    'playwright==1.44.0',
+    'playwright',
     'urllib3==1.26.16',
     'xmltodict',
     'pytz',
